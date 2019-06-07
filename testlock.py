c9383e8bc8cc3069f60ea2a063c4a842b5d616e9
DingZheng2016 <dingzheng214@126.com>
2019-06-07 23:47:54 +0800

    add testlock.py

--- testlock.py
+++ testlock.py
@@ -3,6 +3,9 @@ import threading
 import struct
 import time
 
+ip = '183.172.185.185'
+port = 59898
+
 class MyThread(threading.Thread):
     def __init__(self, arg):
         super(MyThread, self).__init__()
@@ -11,15 +14,16 @@ class MyThread(threading.Thread):
     def run(self):
         sql = "update tb set name='%s' where name='test';"%self.arg
         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.connect(('127.0.0.1', 59898))
+        client.connect((ip, port))
         size = len(sql)
         client.send(struct.pack("!H", size))
         client.send(sql.encode())
         print(client.recv(400).decode())
+        print(client.recv(400).decode())
         time.sleep(2)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 59898))
+client.connect((ip, port))
 
 sql = "use database test;"
 size = len(sql)
@@ -28,34 +32,25 @@ client.send(sql.encode())
 print(client.recv(400).decode())
 print(client.recv(400).decode())
 
+sql = "create table tb (id int, name string(32));"
+size = len(sql)
+client.send(struct.pack("!H", size))
+client.send(sql.encode())
+print(client.recv(400).decode())
+print(client.recv(400).decode())
+
 for i in range(100):
-    sql = "insert into tb values(%d, '%s', %d.%d);"%(i, str(i), i,i)
+    sql = "insert into tb values(%d, 'test');"%i
     size = len(sql)
     client.send(struct.pack("!H", size))
     client.send(sql.encode())
     print(client.recv(400).decode())
     print(client.recv(400).decode())
 
-# for i in range(1000):
-#     sql = "insert into tb values(%d, 'test');"%i
-#     size = len(sql)
-#     client.send(struct.pack("!H", size))
-#     client.send(sql.encode())
-#     print(client.recv(400).decode())
+t1 = MyThread("t1")
+t2 = MyThread("t2")
+t1.start()
+t2.start()
 
-# sql = "update tb where name='test' set name='test_test';"
-# size = len(sql)
-# client.send(struct.pack("!H", size))
-# client.send(sql.encode())
-# while True:
-#     print(client.recv(400).decode())
-# time.sleep(1)
-# t1 = MyThread("t1")
-# t2 = MyThread("t2")
-# t1.start()
-# time.sleep(0.1)
-# t2.start()
-
-while True:
-    pass
-#client.close()
+t1.join()
+t2.join()
